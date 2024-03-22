1140aaa0cb4b939ff6590fb6e18430d9b5e151ca
Jodhansajifab <jodhansaji@gmail.com>
2024-03-22 09:30:41 +0530

    first commit

---
+++ src/lib/elastic-config.ts
@@ -0,0 +1,5 @@
+import { Client } from '@elastic/elasticsearch'
+export const client = new Client({
+    cloud: { id: '483342436e5b4155a4cf16805984f2e0:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJGE5N2Q1OThmMzQ1MTRkZDRhNmQ0MmViZDViODk5YzVhJGE2ZWY0ZmE0Y2E3MTRhYTNhOWZmZGY5YTQwYmI3ZDMy' },
+    auth: { apiKey: 'private-5kend4ujzsjb7bc7vfar36t3' }
+})
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {client} from "$lib/elastic-config";
+    import {onMount} from "svelte";
+
+    onMount(async ()=>{
+        const clientData = await client.info()
+        console.log(clientData)
+    })
+
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
